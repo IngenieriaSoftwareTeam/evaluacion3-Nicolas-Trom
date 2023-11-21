4bf49aae3e03bb04b7e8939a1860471e53eb98ae
Nicolas Trombotti <nicolastrombotti11@gmail.com>
2023-11-21 11:31:17 -0300

    add: test

--- src/user.test.js
+++ src/user.test.js
@@ -1,8 +1,28 @@
-
 import User from './User.js';
 
 const aUser = new User('Ada', 'ada@internet.uy', 20);
+const youngUser = new User('Young', 'young@internet.uy', 17);
 
-test('User over 18 can vote', () => {
+test('Usuario Puede log in', () => {
+  aUser.login();
+  expect(aUser.isLoggedIn()).toBeTruthy();
+});
+
+test('usuario puedo log Out', () => {
+  aUser.logout();
+  expect(aUser.isLoggedIn()).toBeFalsy();
+});
+
+test('User login status is returned correctly', () => {
+  expect(aUser.isLoggedIn()).toBeFalsy();
+  aUser.login();
+  expect(aUser.isLoggedIn()).toBeTruthy();
+});
+
+test('UsuarioPuede Votar', () => {
   expect(aUser.canVote()).toBeTruthy();
+});
+
+test('Usuario No puede votar', () => {
+  expect(youngUser.canVote()).toBeFalsy();
 });
